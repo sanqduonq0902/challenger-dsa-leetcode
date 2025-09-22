38c1b38b4be07dc353caaa61f6bbc9d4e75a08c4
SanqDuonq <namsang0902s@gmail.com>
2025-09-22 18:21:32 +0700

    Leetcode-1071: Solved

---
+++ 1. Array & String/1071. Greatest Common Divisor of Strings.ts
@@ -0,0 +1,10 @@
+function gcdOfStrings(str1: string, str2: string): string {
+    if (str1 + str2 !== str2 + str1) return "";
+
+    const gcd = (a: number, b: number): number => {
+        return b === 0 ? a : gcd(b, a % b);
+    }
+
+    const result = gcd(str1.length, str2.length);
+    return str1.substring(0, result);
+};
