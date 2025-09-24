4db5c81fe52c10eac1ec9f56a7beb281fc7df2e6
SanqDuonq <namsang0902s@gmail.com>
2025-09-24 15:32:55 +0700

    Leetcode-1493: Solved

---
+++ 3. Sliding Window/1493. Longest Subarray of 1's After Deleting One Element.ts
@@ -0,0 +1,23 @@
+function longestSubarray(nums: number[]): number {
+    let n = nums.length;
+    let left = 0, right = 0;
+    let zeroCount = 0;
+    let maxLength = 0;
+
+    for (right = 0; right < n; right++) {
+        if (nums[right] === 0) {
+            zeroCount++;
+        }
+
+        while (zeroCount > 1) {
+            if (nums[left] === 0) {
+                zeroCount--;
+            }
+            left++;
+        }
+
+        maxLength = Math.max(maxLength, right - left);
+    }
+
+    return maxLength;
+};
