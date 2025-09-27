f250246022dbb09bb1d29f1281de010d0237d8bc
SanqDuonq <namsang0902s@gmail.com>
2025-09-27 21:38:51 +0700

    Leetcode-206: Solved

---
+++ 8. Linked List/206. Reverse Linked List.ts
@@ -0,0 +1,14 @@
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let curr = head;
+    
+
+    while (curr !== null) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+};
