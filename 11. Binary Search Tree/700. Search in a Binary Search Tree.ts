a74d3d4a7f387b039a0a14cf0baf0d0595753ba6
SanqDuonq <namsang0902s@gmail.com>
2025-09-28 19:54:53 +0700

    Leetcode-700: Solved

---
+++ 11. Binary Search Tree/700. Search in a Binary Search Tree.ts
@@ -0,0 +1,8 @@
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (!root) return null;
+
+    if (root.val === val) return root;
+
+    if (root.val > val) return searchBST(root.left, val);
+    else return searchBST(root.right, val);
+}; 
