7519e4bac3f982689f22cc477387023dc575fe41
SanqDuonq <namsang0902s@gmail.com>
2025-10-04 19:55:12 +0700

    Leetcode-374: Solved

---
+++ 15. Binary Search/374. Guess Number Higher or Lower.ts
@@ -0,0 +1,19 @@
+function guessNumber(n: number): number {
+    let left = 0;
+    let right = n;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const result = guess(mid);
+        
+        if (result === 0) return mid;
+        else if (result === -1) {
+            right = mid - 1;
+        } 
+        else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
