d0cb20672c6d6bc935ef7019ff0b3a10747a0852
SanqDuonq <namsang0902s@gmail.com>
2025-10-05 16:27:28 +0700

    Leetcode-216: Solved

---
+++ 16. Backtracking/216. Combination Sum III.ts
@@ -0,0 +1,21 @@
+function combinationSum3(k: number, n: number): number[][] {
+  const result: number[][] = [];
+
+  function backtrack(start: number, path: number[], sum: number) {
+    if (path.length === k) {
+      if (sum === n) result.push([...path]);
+      return;
+    }
+
+    for (let i = start; i <= 9; i++) {
+      if (sum + i > n) break;
+
+      path.push(i);
+      backtrack(i + 1, path, sum + i); 
+      path.pop(); 
+    }
+  }
+
+  backtrack(1, [], 0);
+  return result;
+}
