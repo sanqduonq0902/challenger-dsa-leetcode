300256c0e662e7600861ffd5fe7e91eb8c740fa5
SanqDuonq <namsang0902s@gmail.com>
2025-10-06 22:21:10 +0700

    Leetcode-901: Solved

---
+++ 22. Monotonic Stack/901. Online Stock Span.ts
@@ -0,0 +1,18 @@
+class StockSpanner {
+  private stack: [number, number][]; 
+
+  constructor() {
+    this.stack = [];
+  }
+
+  next(price: number): number {
+    let span = 1;
+
+    while (this.stack.length && this.stack[this.stack.length - 1][0] <= price) {
+      span += this.stack.pop()![1];
+    }
+
+    this.stack.push([price, span]);
+    return span;
+  }
+}
