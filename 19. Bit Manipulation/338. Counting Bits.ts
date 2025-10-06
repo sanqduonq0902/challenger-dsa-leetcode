89f94e2c375d5a31b7599b2af0f03729383a761c
SanqDuonq <namsang0902s@gmail.com>
2025-10-06 21:32:54 +0700

    Leetcode-338: Solved

---
+++ 19. Bit Manipulation/338. Counting Bits.ts
@@ -0,0 +1,9 @@
+function countBits(n: number): number[] {
+    const result: number[] = Array(n + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        result[i] = result[i >> 1] + (i & 1);
+    }
+
+    return result;
+};
